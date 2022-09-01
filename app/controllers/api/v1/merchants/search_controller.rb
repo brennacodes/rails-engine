1307ed18efffc8f4c8eba61f9da9f7a1278a8117
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-01 17:46:44 -0600

    Update merchant search controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,42 +2,49 @@ module Api
   module V1
     module Merchants
       class SearchController < ApplicationController
-        # This function takes in a name parameter and returns a list of merchants that match the name
         def index
-          merchants = Merchant.find_all_by_input(params[:name])
-          serialize_merchant(merchants)
+          if nil_check == true
+            json_missing_input
+          elsif check_input == nil
+            check = check_input
+            json_not_found(check[0], check[1])
+          else
+            merchant = Merchant.find_all_by_input(params[:name])
+            return json_not_found("name", check_input[1]) if merchant == nil
+            serialize_merchant(merchant)
+          end
         end
 
-        # If the params hash has an id key, find the merchant by id, 
-        # otherwise find the merchant by name
         def show
-          check = check_input
-          if check == nil
-            return json_missing_input
-          elsif params[:id]
-            merchant = Merchant.find(params[:id])
-            serialize_merchant(merchant)
-          elsif params[:name]
-            require 'pry'; binding.pry 
-            merchant = Merchant.find_by_input(params[:name])
-            serialize_merchant(merchant)
+          if nil_check == true
+            json_missing_input
+          elsif check_input == nil
+            check = check_input
+            json_not_found(check[0], check[1])
           else
-            return json_not_found(check[0] || check[1])
+            merchant = Merchant.find_by_input(params[:name])
+            return json_not_found("name", params[:name]) if merchant == nil
+            serialize_merchant(merchant)
           end
         end
 
         private
-          # It returns an array of the type of input and the input itself, 
-          # or nil if either is empty
+          def nil_check
+            return true if check_input == nil
+          end
+
+          # It checks to see if the user has entered any search parameters, 
+          # and if not, it returns a 204 status code and an error message
           def check_input
-            type = find_type
-            input = params[type.to_sym]
-            type.empty? || input.empty? ? nil : [type, input]
+            return nil if params[:name] == nil
+            return nil if params[:name].empty?
+            ["name", params[:name]]
           end
 
           def merchant_params
-            params.require(:merchant).permit(:name)
+            params.permit(:name)
           end
+        
       end
     end
   end
