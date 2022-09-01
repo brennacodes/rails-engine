ce91a15bd5825e46566d9b28eb006079636f89f4
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-01 17:56:37 -0600

    Update merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,9 +7,19 @@ module Api
       end
 
       def show
-        merchant = Merchant.find(params[:id])
-        serialize_merchant(merchant)
+        if Merchant.exists?(params[:id])
+          serialize_merchant(Merchant.find(params[:id]))
+        elsif !Merchant.exists?(params[:id])
+          json_not_found('id', params[:id])
+        else
+          json_missing_input
+        end
       end
+
+      private
+        def merchant_params
+          params.permit(:id, :name)
+        end
     end
   end
 end
